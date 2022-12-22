4ec962affbcfb343a4b8ca5ccc77396c2b167426
Lea Lescure <lea.lescure@hotmail.fr>
2022-12-22 18:29:00 +0100

    Initial Commit

---
+++ skydiving weather/skydiving_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  skydiving_weatherApp.swift
+//  skydiving weather
+//
+//  Created by Léa Lescure on 22/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct skydiving_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
